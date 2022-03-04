3430f4e8ae3c448137ada4bde33dba6e65046820
Harry Patlow <ghp.coding@gmail.com>
2022-03-04 19:23:06 +0530

    Add Django Models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
+from distutils.command.upload import upload
 from django.db import models
+from matplotlib.image import thumbnail
 
 # Create your models here.
+
+class Article(models.Model):
+    thumbnail = models.ImageField(upload_to = "uploads/", blank = True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=64)
